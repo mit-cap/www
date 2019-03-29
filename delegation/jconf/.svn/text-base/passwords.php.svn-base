c01333d709f3aa25c43afcb51f0ab99fefdaf5f4
Jack Feser <feser@csail.mit.edu>
2019-03-29 00:23:13 -0400

    Remove unused Jeeves site.

--- delegation/jconf/.svn/text-base/passwords.php.svn-base
+++
@@ -1,7 +0,0 @@
-<?php
-$USERS["jeanyang"] = "jeanyang";
-$USERS["armando"] = "armando";
-$USERS["jzim"] = "jzim";
-$USERS["justin"] = "justin";
-$USERS["admin"] = "";
-?>
--- delegation/socialnet/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include 'passwords.php';
-check_logged();
-session_unset();
-session_destroy();
-echo 'You are logged out.<br>';
-echo '<a href="login.php">Log in</a>';
-?>
